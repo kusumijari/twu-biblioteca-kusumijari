0cb68f960c3b75db65734ac1092f8d0953b0eacc
vagrant <vagrant@precise64.(none)>
2015-09-14 12:36:29 +0000

    Added spec to get movie list in the list movies class

--- src/com/tw/Biblioteca/ListMovies.java
+++ src/com/tw/Biblioteca/ListMovies.java
@@ -3,4 +3,14 @@
 package com.tw.biblioteca;
 
 public class ListMovies {
+
+    private MovieLibrary movieLibrary;
+
+    public ListMovies(MovieLibrary movieLibrary) {
+        this.movieLibrary = movieLibrary;
+    }
+
+    public String getMovies() {
+        return movieLibrary.toString();
+    }
 }
--- src/com/tw/Biblioteca/MovieLibrary.java
+++ src/com/tw/Biblioteca/MovieLibrary.java
@@ -23,6 +23,5 @@ public class MovieLibrary {
             stringBuilder.append(movie.toString()+"\n");
         }
         return stringBuilder.toString();
-
     }
 }
