44a558e833cdba82dbf6cb94c5302932fe238d3f
vagrant <vagrant@precise64.(none)>
2015-09-15 11:40:53 +0000

    Added a spec to check for an invalid user

--- src/com/tw/Biblioteca/Login.java
+++ src/com/tw/Biblioteca/Login.java
@@ -1,33 +1,39 @@
-//Asks for the login and password and authenticates it
+//Asks for the login and password creates a user to be checked
 
 package com.tw.biblioteca;
+
 
 import java.util.ArrayList;
 
 public class Login {
 
+    User user;
+    ConsoleInput consoleInput1, consoleInput2;
     ArrayList<User> users = new ArrayList<User>();
 
-     Login() {
-        User user1 = new User("Customer", "123-4567", "password");
-        User user2 = new User("Librarian", "123-4568", "password");
+    public Login(ConsoleInput consoleInput1, ConsoleInput consoleInput2) {
+        this.consoleInput1 = consoleInput1;
+        this.consoleInput2 = consoleInput2;
+        User user1 = new User("Role", "123-4567", "password1");
+        User user2 = new User("role", "111-1111", "password2");
         users.add(user1);
         users.add(user2);
     }
 
     public String acceptUsername(ConsoleInput consoleInput) {
-        ConsoleOutput consoleOutput = new ConsoleOutput("Enter Username:");
-        consoleOutput.displayMessage();
         return consoleInput.getInput();
     }
 
     public String acceptPassword(ConsoleInput consoleInput) {
-        ConsoleOutput consoleOutput = new ConsoleOutput("Enter Password:");
-        consoleOutput.displayMessage();
         return consoleInput.getInput();
     }
 
+
     public boolean authenticate() {
-        return true;
+        User user = new User("role", acceptUsername(consoleInput1), acceptPassword(consoleInput2));
+        if(users.contains(user)) {
+            return true;
+        }
+        return false;
     }
 }
--- test/com/tw/biblioteca/LoginTest.java
+++ test/com/tw/biblioteca/LoginTest.java
@@ -2,8 +2,7 @@ package com.tw.biblioteca;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -11,7 +10,7 @@ public class LoginTest {
 
     @Test
     public void shouldAcceptUsernameFromUser() {
-        Login login = new Login();
+        Login login = new Login(new ConsoleInput(), new ConsoleInput());
         ConsoleInput mockConsoleInput = mock(ConsoleInput.class);
 
         when(mockConsoleInput.getInput()).thenReturn("123-4567");
@@ -21,7 +20,7 @@ public class LoginTest {
 
     @Test
     public void shouldAcceptPasswordFromUser() {
-        Login login = new Login();
+        Login login = new Login(new ConsoleInput(), new ConsoleInput());
         ConsoleInput mockConsoleInput = mock(ConsoleInput.class);
 
         when(mockConsoleInput.getInput()).thenReturn("password");
@@ -30,12 +29,37 @@ public class LoginTest {
     }
 
     @Test
-    public void shouldReturnTrueIfTheEnteredUsernameIsAValidUsername() {
-        Login login = new Login();
-        ConsoleInput mockConsoleInput = mock(ConsoleInput.class);
+    public void shouldCheckIftheUserAndPasswordInformationIsValid() {
+        ConsoleInput mockConsoleInput1 = mock(ConsoleInput.class);
+        ConsoleInput mockConsoleInput2 = mock(ConsoleInput.class);
+        Login login = new Login(mockConsoleInput1, mockConsoleInput2);
 
-        when(mockConsoleInput.getInput()).thenReturn("123-4567", "password");
+        when(mockConsoleInput1.getInput()).thenReturn("123-4567");
+        when(mockConsoleInput2.getInput()).thenReturn("password");
+
+        login.acceptUsername(mockConsoleInput1);
+
+        login.acceptPassword(mockConsoleInput2);
 
         assertTrue(login.authenticate());
     }
+
+    @Test
+    public void shouldCheckIftheUserAndPasswordInformationIsInvalid() {
+        ConsoleInput mockConsoleInput1 = mock(ConsoleInput.class);
+        ConsoleInput mockConsoleInput2 = mock(ConsoleInput.class);
+        Login login = new Login(mockConsoleInput1, mockConsoleInput2);
+
+        when(mockConsoleInput1.getInput()).thenReturn("123-4568");
+        when(mockConsoleInput2.getInput()).thenReturn("password");
+
+
+        login.acceptUsername(mockConsoleInput1);
+
+
+        login.acceptPassword(mockConsoleInput2);
+
+        assertFalse(login.authenticate());
+    }
+
 }
