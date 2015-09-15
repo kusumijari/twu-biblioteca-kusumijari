74434bbb95515d43c589900e483571d72787fee8
vagrant <vagrant@precise64.(none)>
2015-09-15 16:56:22 +0000

    Added spec to return true if username is incorret

--- src/com/tw/Biblioteca/Authenticator.java
+++ src/com/tw/Biblioteca/Authenticator.java
@@ -2,15 +2,25 @@
 
 package com.tw.biblioteca;
 
+import java.util.ArrayList;
+
 public class Authenticator {
 
     private User user;
+    private ArrayList<User> users = new ArrayList<User>();
 
     public Authenticator(User user) {
         this.user = user;
+        User user1 = new User("Customer", "123-4567", "password1");
+        User user2 = new User("Librarian", "111-1111", "password2");
+        users.add(user1);
+        users.add(user2);
     }
 
     public boolean authenticate() {
-        return true;
+        if(users.contains(user)){
+            return true;
+        }
+        return false;
     }
 }
--- test/com/tw/biblioteca/AuthenticatorTest.java
+++ test/com/tw/biblioteca/AuthenticatorTest.java
@@ -2,6 +2,7 @@ package com.tw.biblioteca;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AuthenticatorTest {
@@ -13,5 +14,12 @@ public class AuthenticatorTest {
         assertTrue(authenticator.authenticate());
     }
 
-    
+    @Test
+    public void shouldCheckIftheUsernameIsInvalid() {
+        Authenticator authenticator =  new Authenticator(new User("role", "123-4568", "password"));
+
+        assertFalse(authenticator.authenticate());
+    }
+
+
 }
