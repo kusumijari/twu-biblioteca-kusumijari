7ce703818c8a58ea95a1ee1ce3a7a2069756ed4f
vagrant <vagrant@precise64.(none)>
2015-09-16 18:47:47 +0000

    Added spec to display the user details

--- src/com/tw/Biblioteca/Authenticater.java
+++
@@ -1,7 +0,0 @@
-package com.tw.biblioteca;
-
-/**
- * Created by kusumi on 15/09/15.
- */
-public class Authenticater {
-}
--- src/com/tw/Biblioteca/MoiveLibrary.java
+++
@@ -1,5 +0,0 @@
-package com.tw.biblioteca;
-
-public class MoiveLibrary {
-
-}
